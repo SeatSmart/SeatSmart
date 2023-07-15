d0181afc5a8a546fd13e0d59ab7f4a792a7eac53
SHRAVAN D A <shravan8704@gmail.com>
2023-07-16 02:55:29 +0530

    useEffect
    used useEffect hook to fetch the current data of state variable and do proper calculation

--- src/Components/examcell3.jsx
+++ src/Components/examcell3.jsx
@@ -1,16 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import "./ex3.css"
 const Examcell3 = () => {
-    const [inputValue, setInputValue] = useState('');
+    var vacant_cs1=[];var vacant_cs2=[];var vacant_ee1=[];var vacant_ee2=[];var vacant_ec1=[];var vacant_ec2=[];var vacant_ce1=[];var vacant_ce2=[];var vacant_me1=[];var vacant_me2=[];
+    const [inputValues, setInputValues] = useState('');
+    const [vacant_cs1Length, setVacant_cs1Length] = useState('');
+    const [cs1, setCs1] = useState('');
+    const [vacant_cs2Length, setVacant_cs2Length] = useState('');
+    const [cs2, setCs2] = useState('');
+    const [vacant_ee1Length, setVacant_ee1Length] = useState('');
+    const [ee1, setee1] = useState('');
+    const [vacant_ee2Length, setVacant_ee2Length] = useState('');
+    const [ee2, setee2] = useState('');
+    const [vacant_ec1Length, setVacant_ec1Length] = useState('');
+    const [ec1, setec1] = useState('');
+    const [vacant_ec2Length, setVacant_ec2Length] = useState('');
+    const [ec2, setec2] = useState('');
+    const [vacant_ce1Length, setVacant_ce1Length] = useState('');
+    const [ce1, setce1] = useState('');
+    const [vacant_ce2Length, setVacant_ce2Length] = useState('');
+    const [ce2, setce2] = useState('');
+    const [vacant_me1Length, setVacant_me1Length] = useState('');
+    const [me1, setme1] = useState('');
+    const [vacant_me2Length, setVacant_me2Length] = useState('');
+    const [me2, setme2] = useState('');
 
-    const handleInputChange = (event) => {
-      setInputValue(event.target.value);
+    useEffect(() => {
+        
+    },[cs1,vacant_cs1Length]);
+
+    const handleInputChange_cs1 = (event) => {
+        const value = event.target.value;
+        // console.log(value);
+        setCs1(value);
+        // console.log(cs1);
+    };
+    const handleInputvacant_cs1 = (event) =>{
+        const value = event.target.value;
+        setInputValues(value);
+        vacant_cs1 = value.split(',').map((val) => val.trim());
+        console.log(vacant_cs1);
+        setVacant_cs1Length(vacant_cs1.length);
+        // console.log(vacant_cs1Length);
+        
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setCs1(cs1 - vacant_cs1Length);
     };
     return (
         <div className='ex3'>
             <h1>EXAM CELL</h1>
             <div className="table">
-                <form onSubmit={handleInputChange}>
+                <form onSubmit={handleSubmit}>
                     <table>
                         <tr>
                             <th>CLASS</th>
@@ -29,13 +71,23 @@ const Examcell3 = () => {
                                         1
                                     </p>
                                     to
-                                    <input className='roll1' type="text" value={inputValue} onChange={handleInputChange} />
+                                    <input className='roll1' type="text" value={cs1}  onInput={handleInputChange_cs1} />
                                 </div>
-                            </td>                 
+                            </td>
+                            <td>
+                                <input className='classlabel' type='text' value={inputValues} onInput={handleInputvacant_cs1} />    
+                            </td>    
+                            <td>
+                                <p className='classlabel'>course</p>
+                            </td>   
+                            <td>
+                                <p className='classlabel'>{cs1}</p>    
+                            </td>         
                         </tr>
                     </table>
-                    <button type='submit' className='classlabel'>Continue</button>
+                    <button type='submit' className='classlabel'>Submit</button>
                 </form>
+
             </div>
         </div>
     )
