fc7b1a0d2e3e827c84da8c4aa0ae536a7b6b8bac
SHRAVAN D A <shravan8704@gmail.com>
2023-07-18 00:32:19 +0530

    hmm

--- src/Components/examcell4.jsx
+++ src/Components/examcell4.jsx
@@ -3,15 +3,31 @@ import './ex4.css';
 import { useState } from 'react';
 import { RoomData } from '../Data/Rooms';
 const Examcell4 = () => {
+    const [rooms, setRooms] = useState(RoomData);
+
+    const handleButtonClick = (id) => {
+      setRooms((prevRooms) =>
+        prevRooms.map((room) => {
+          if (room.id === id) {
+            return { ...room, selected: !room.selected };
+          }
+          return room;
+        })
+      );
+    };
     return ( 
         <div className='ex4'>
             <h1>EXAM CELL</h1>
             <div className="available">
-            {RoomData.map((item) => {
-                <button className="room-button" >
-                    {item.name}
-                </button>
-            })}
+            {rooms.map((item) => (
+            <button
+                key={item.id}
+                className={`room-button ${item.selected ? 'selected' : ''}`}
+                onClick={() => handleButtonClick(item.id)}
+            >
+                {item.name}
+            </button>
+            ))}
             </div>
         </div>
      );
