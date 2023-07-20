ef0ae812e56bdc0a694b83913dea7f8627a070dd
K S MITHUN <mithunmpire@gmail.com>
2023-07-20 12:42:18 +0530

    changes

--- src/Components/Invigilator2.jsx
+++ src/Components/Invigilator2.jsx
@@ -4,7 +4,9 @@ import { useState,useEffect,Link } from 'react';
 const Invigilator2 = () =>{
     const [clicked, setClicked] = useState(null);
     const roomData = AllotedData[0];
+    // const roomData2 = AllotedData[1];
     const [selectedRolls, setSelectedRolls] = useState([]);
+    // const [selectedRolls2, setSelectedRolls2] = useState([]);
 
     const handleButtonClick = (roll) => {   
         setClicked((prevClicked) => (prevClicked === roll ? null : roll));
@@ -18,14 +20,28 @@ const Invigilator2 = () =>{
             setSelectedRolls((prevSelectedRolls) => [...prevSelectedRolls, roll]);
           }
       };
+      // const handleButtonClick1 = (roll) => {   
+      //   setClicked1((prevClicked1) => (prevClicked1 === roll ? null : roll));
+      //   if (selectedRolls.includes(roll)) {
+            
+      //       setSelectedRolls2((prevSelectedRolls2) =>
+      //         prevSelectedRolls2.filter((selectedRoll1) => selectedRoll1 !== roll)
+      //       );
+      //     } else {
+            
+      //       setSelectedRolls2((prevSelectedRolls2) => [...prevSelectedRolls2, roll]);
+      //     }
+      // };
       useEffect(() => {
         setSelectedRolls(roomData.cs1);
+        // setSelectedRolls2(roomData2.cs1);
       }, [roomData.cs1]);
 
     return(
         <div className="inv2">
             <h1 className='heading'>Invigilator</h1>
-            <h2 className='roomno'>Room No: A301</h2>
+            <h2 className='cs1'>Room No: A301</h2>
+            <h3 className='cs1'> CSE1</h3>
             {roomData.cs1.map((roll, index) => {
         return (
           <button
@@ -38,6 +54,20 @@ const Invigilator2 = () =>{
           
         );
       })}
+
+{/* <h3 className='cs1'> ME1</h3>
+            {roomData.cs1.map((roll, index) => {
+        return (
+          <button
+            className={`roll-btn ${selectedRolls2.includes(roll) ? "checked" : ""}`}
+            key={index}
+            onClick={() => handleButtonClick1(roll)}
+          >
+            {roll}
+          </button>
+          
+        );
+      })} */}
       <button className='continue-button'>Submit</button>
         </div>
     )
